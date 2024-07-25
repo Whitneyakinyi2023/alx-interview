c537b7ef6bac9d250629414db5123b88b866407f
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-07-26 00:36:04 +0300

    Pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """pascal"""
+
+
 def pascal_triangle(n):
     """
     Generates Pascal's triangle up to a specified number of rows (n).
@@ -19,6 +21,7 @@ def pascal_triangle(n):
     """Row ends is 1"""
 
     for i in range(1, n):
+        """requirem"""
         row = [1]
         """Each row starts with 1
         Compute the middle values based on the previous row"""
@@ -26,6 +29,7 @@ def pascal_triangle(n):
         for j in range(1, i):
             """Traversal"""
             row.append(previous_row[j - 1] + previous_row[j])
+            """End row wirth 1"""
         row.append(1)
         triangle.append(row)
 
