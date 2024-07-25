e220dbf099da0e2a30147398bdb192b7b6823d6c
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-07-26 00:25:46 +0300

    Pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""pascal"""
 def pascal_triangle(n):
     """
     Generates Pascal's triangle up to a specified number of rows (n).
@@ -12,9 +12,11 @@ def pascal_triangle(n):
         Pascal's triangle, or an empty list if n <= 0.
     """
     if n <= 0:
+        """Returns an empty list"""
         return []
 
-    triangle = [[1]]  # The first row is always [1]
+    triangle = [[1]]
+    """Row ends is 1"""
 
     for i in range(1, n):
         row = [1]
@@ -23,7 +25,7 @@ def pascal_triangle(n):
         previous_row = triangle[i - 1]
         for j in range(1, i):
             row.append(previous_row[j - 1] + previous_row[j])
-        row.append(1)  # Each row ends with 1
+        row.append(1)
         triangle.append(row)
 
     return triangle
